444bbb87ea72fcd2cfd2f141dc6e32d3616cc71f
Sharon Watkins <swatkins876@gmail.com>
2019-09-12 16:12:25 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+katz_deli = []
+
+ def line(array)
+  if array.length == katz_deli
+    puts "This line is currently empty."
+  end
+  
+  def take_a_number(array, name)
+    array.collect { |person| puts "#{name} is #{index[-1]} in line" }
+  end
+  
+  def now_serving(line)
+  if line.empty?
+    puts"There is nobody waiting to be served!"
+  else
+    puts "Serving #{line[-1]}."
+    line.shift
+  end
