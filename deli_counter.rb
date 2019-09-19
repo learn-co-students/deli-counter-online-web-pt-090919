366412bf2dcd70ebbcb055d007c58e35a7e4ad8f
Jamal Turner <jlsmitty2273@yahoo.com>
2019-09-19 18:23:29 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def line(array)
+  
+  if array == []
+  puts "The line is currently empty."
+  elsif 
+  phrase = "The line is currently:"
+  array.each_with_index do |name, index|
+  phrase = phrase + " #{index + 1}. #{name}"
+  
+  end
+  puts phrase
+  end
+end
+
+#def take_a_number(array,name)
+ # puts "the "
+#end
