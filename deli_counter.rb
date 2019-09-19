c298b503c4286190720114c3439fd8da2f3f0c74
Jamal Turner <jlsmitty2273@yahoo.com>
2019-09-19 18:37:54 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,16 @@
 def line(array)
-  
-  if array == []
+  array == []
   puts "The line is currently empty."
-  elsif 
   phrase = "The line is currently:"
   array.each_with_index do |name, index|
   phrase = phrase + " #{index + 1}. #{name}"
-  
+
   end
   puts phrase
   end
 end
 
-#def take_a_number(array,name)
- # puts "the "
-#end
+def take_a_number(array, name)
+  array << name
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+  end
