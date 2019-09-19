7bcec9b731b08629a7cc7f3e0dbbabbfe7034cb2
Jamal Turner <jlsmitty2273@yahoo.com>
2019-09-19 18:47:23 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
 def line(array)
-  array == []
+  array = []
   puts "The line is currently empty."
   phrase = "The line is currently:"
   array.each_with_index do |name, index|
@@ -7,8 +7,6 @@ def line(array)
 
   end
   puts phrase
-  end
-end
 
 def take_a_number(array, name)
   array << name
