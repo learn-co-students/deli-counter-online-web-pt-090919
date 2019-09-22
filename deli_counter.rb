596736840cb03e8fcb398270f3ca85a201b015c3
Jalysa Reason <jalysareason@gmail.com>
2019-09-22 11:07:05 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def #Deli_Counter 
+  let(:katz_deli) { [] }
+  let(:other_deli) { ["Logan", "Avi", "Spencer"] }
+  let(:another_deli) { ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"] }
+end
+
+  def #Deli_Counter 
+    puts= line is empty
+        line(katz_deli)=("The line is currently empty.")
+    end
+
+    
